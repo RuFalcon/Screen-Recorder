58f6bb92241c8c5831f05ea96076fea52113e8a8
RuFalcon <sokolovmn123@yandex.ru>
2022-01-12 14:23:39 +0300

    1 file modified

--- dist/main.js
+++ dist/main.js
@@ -80,7 +80,7 @@ function handleStop(e){
   chunks = []
 
   downloadButton.href = URL.createObjectURL(blob)
-  downloadButton.download = 'video.webm'
+  downloadButton.download = 'video.mp4'
   downloadButton.disabled = false
 
   recordedVideo.src = URL.createObjectURL(blob)
